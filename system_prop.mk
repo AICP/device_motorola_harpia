87d6044885cf98469e45e796356703e511ca5e18
Kyle Harrison <khwebmail@gmail.com>
2020-04-19 20:00:50 +0530

    harpia: Migrate from system.prop to system_prop.mk
    Change-Id: I7ff7dbf0f9f240eb1b38a1b878ede8f331223f88

---
+++ system_prop.mk
@@ -0,0 +1,17 @@
+# system.prop for harpia
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.mode=endfire \
+    persist.vendor.audio.fluence.voicecall=true \
+    ro.vendor.audio.sdk.fluencetype=fluence
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false \
+    ro.sf.lcd_density=320
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=10
+
